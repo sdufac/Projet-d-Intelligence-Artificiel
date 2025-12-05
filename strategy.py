8ba3d800abaee180fee8f3a07ad6b7d1342b9409
TeZaGe <thomas.modesto@gmail.com>
2025-12-05 15:28:31 +0100

    MCTS

--- strategy.py
+++ strategy.py
@@ -1,5 +1,5 @@
 from logging import info
-from math import inf
+import math
 from time import process_time_ns
 from typing import List, Optional
 from logic import GameState, Move, apply_move
@@ -11,6 +11,7 @@ from logic import _check_induced_path_property
 from typing import List, Optional, Dict, Set
 from logic import GameState, Move, get_legal_moves
 import random
+import sys
 
 # This file as well as utils.py should be the only ones you have to edit!
 
@@ -116,11 +117,114 @@ class MinMaxStrategy(Strategy):
                 v = v2
                 move = a
         return (v,move)
+    
+
+class MCTSNode:
+    def __init__(self, state: GameState, parent=None, move= None, player_to_move=0):
+        self.state = state
+        self.parent = parent
+        self.move = move
+        self.player_to_move = player_to_move
+        self.children = []
+        self.wins = 0
+        self.visits = 0
+
+        self.untried_moves = get_legal_moves(state, state.G, player_to_move)
+class MonteCarloTreeSearchStrategy(Strategy):
+        '''
+        Réalisation avec l'aide de Gemini
+        '''
+
+
+        def select_move(self, state: GameState, G: Dict[int, Set[int]], player: int) -> Optional[Move]:
+            iter = 1000
+            start = MCTSNode(state=state, parent=None, move=None, player_to_move=player)
+
+            if not start.untried_moves:
+                return None
+            
+            for i in range(iter):
+                node = self.selection(start, player)
+                node = self.expansion(node, player)
+                winner = self.simulation(node.state, node.player_to_move)
+                self.backpropagation(node, winner, player)
+
+            if not start.children:
+                return None
+            
+            # On prend l'enfant avec le plus de visites 
+            best_child = max(start.children, key=lambda c: c.visits)
+            return best_child.move
+        
+        def selection(self, node: MCTSNode, player: int) -> MCTSNode:
+            # tant que enfant et pas de coup non essayé
+            while node.children and not node.untried_moves:
+                node = self.get_best_child(node, player)
+            return node
+        
+        def get_best_child(self, node: MCTSNode, player: int) -> MCTSNode:
+
+            best_score = -float('inf')
+            best_child = None
+
+            for child in node.children:
+                #UCB1(i) = winrate + C * sqrt(ln(N) / n)
+                score = (child.wins / child.visits) + math.sqrt(2) * math.sqrt(math.log(node.visits) / child.visits)
+                
+                # choix du meilleur enfant
+                if score > best_score:
+                    best_score = score
+                    best_child = child
+            return best_child
+        
+        def expansion(self, node: MCTSNode, player: int) -> MCTSNode:
+
+            if not node.untried_moves:
+                return node 
+            
+            # on choisit un coup non encore essayé
+            move = node.untried_moves.pop()
+            new_state = copy.deepcopy(node.state)
+
+            apply_move(new_state, node.player_to_move, move)
+
+            next_player = 1 - node.player_to_move
+            child_node = MCTSNode(state=new_state, parent=node, move=move, player_to_move=next_player)
+            node.children.append(child_node)
+            return child_node
+
+        
+        def simulation(self, state: GameState, player: int) -> int:
+            
+            current_state = copy.deepcopy(state)
+            current_player = player
+
+            while True:
+                legal_moves = get_legal_moves(current_state, current_state.G, current_player)
+                
+                if not legal_moves:
+                    return 1 - current_player  # L'autre joueur a gagné
+
+                move = random.choice(legal_moves)
+                apply_move(current_state, current_player, move)
+                current_player = 1 - current_player
+        
+        def backpropagation(self, node: MCTSNode, winner: int, player: int):
+            while node is not None:
+                node.visits += 1
+                
+                player_moved = 1 - node.player_to_move
+
+                if winner == player_moved:
+                    node.wins += 1
+
+                node = node.parent
 
 # Registry of available strategies.
 # Add your new strategies here to run them from main.py.
 STRATEGIES = {
     "random": RandomStrategy,
     "greedy": GreedyMaxDegreeStrategy,
-    "minmax": MinMaxStrategy
+    "minmax": MinMaxStrategy,
+    "mcts": MonteCarloTreeSearchStrategy
 }
